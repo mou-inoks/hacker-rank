d41207a8545740994ff97047b148ab82e88c020f
mou-inoks <mou@inokscapital.com>
2024-02-28 19:34:16 +0100

    refactor

--- compare-the-tripplet.cs
+++ compare-the-tripplet.cs
@@ -41,18 +41,17 @@ class Result
 
     public static List<int> compareTriplets(List<int> a, List<int> b)
     {
-        var ap = 0;
-        var bp = 0;
+        var t = new List<int>{0,0};
         
         for(int i = 0; i < a.Count; i++)
         {
             if(a[i] > b[i])
-                ap++;
+                t[0]++;
             else if (a[i] < b[i])
-                bp++;
+                t[1]++;
         }
         
-        return new List<int>{ap, bp};
+        return t;
 
     }
 
